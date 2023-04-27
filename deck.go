cd641b04bb863088fa6c744972c4a76dd6fa905b
GuibuAdrian <guibuadrian@gmail.com>
2023-04-27 12:30:25 -0300

    Added comment about receiver convention

--- deck.go
+++ deck.go
@@ -6,6 +6,7 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+// We never use the words this or self by convention we usually refer to the receiver with a single letter
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
