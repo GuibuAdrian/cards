2077ab5ca300f44ff1dc55017c35f71417d7e523
GuibuAdrian <guibuadrian@gmail.com>
2023-05-02 14:41:15 -0300

    more complex tests

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -13,5 +17,26 @@ func TestNewDeck(t *testing.T) {
 	}
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected first card of Four of Clubs, but got %v\n", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	d := newDeck()
+	err := d.saveToFile("_decktesting")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v\n", len(loadedDeck))
+	}
+
+	err = os.Remove("_decktesting")
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 }
